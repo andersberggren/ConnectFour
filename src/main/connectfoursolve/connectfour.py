dca4ef7aa2e6df39af89f9df35151809eae3d3eb
Anders Berggren <anders.berggren@acino.se>
2019-01-08 17:36:26 +0100

    Unit tests for ConnectFour class

--- src/main/connectfoursolve/connectfour.py
+++ src/main/connectfoursolve/connectfour.py
@@ -9,6 +9,8 @@ class ConnectFour:
 			self.place_disc(self.move)
 	
 	def place_disc(self, column):
+		if column < 0 or column >= ConnectFour.width:
+			raise ValueError("Invalid column: {}".format(column))
 		row_index = 0
 		while (column, row_index) in self.position_to_disc:
 			row_index += 1
@@ -23,27 +25,27 @@ class ConnectFour:
 	def get_winner(self):
 		# Vertical
 		for x in range(ConnectFour.height):
-			winner = self.get_winner_one_direction(x, 0, (0,1))
+			winner = self._get_winner_one_direction(x, 0, (0,1))
 			if winner is not None:
 				return winner
 		# Horizontal
 		for y in range(ConnectFour.width):
-			winner = self.get_winner_one_direction(0, y, (1,0))
+			winner = self._get_winner_one_direction(0, y, (1,0))
 			if winner is not None:
 				return winner
 		# Diagonal /
 		for x in range(4-ConnectFour.height, ConnectFour.width+1-4):
-			winner = self.get_winner_one_direction(x, 0, (1,1))
+			winner = self._get_winner_one_direction(x, 0, (1,1))
 			if winner is not None:
 				return winner
 		# Diagonal \
 		for x in range(4-ConnectFour.height, ConnectFour.width+1-4):
-			winner = self.get_winner_one_direction(x, ConnectFour.height-1, (1,-1))
+			winner = self._get_winner_one_direction(x, ConnectFour.height-1, (1,-1))
 			if winner is not None:
 				return winner
 		return None
 	
-	def get_winner_one_direction(self, x_start, y_start, direction):
+	def _get_winner_one_direction(self, x_start, y_start, direction):
 		x = x_start
 		y = y_start
 		player = None
@@ -65,10 +67,18 @@ class ConnectFour:
 			y += direction[1]
 		return None
 	
+	def get_heuristic_value(self):
+		winner = self.get_winner()
+		if winner is not None:
+			value = 1000000 + ConnectFour.width*ConnectFour.height - len(self.position_to_disc)
+			return value if winner == 0 else -value
+		else:
+			return 0
+	
 	def get_state_as_string(self):
 		symbols = ["X", "O"]
 		s = ""
-		for y in range(ConnectFour.height):
+		for y in range(ConnectFour.height-1, -1, -1):
 			for x in range(ConnectFour.width):
 				try:
 					s += symbols[self.position_to_disc[(x,y)]]
---
+++ src/test/connectfoursolve/tests.py
@@ -0,0 +1,102 @@
+import unittest
+
+from connectfoursolve.connectfour import ConnectFour
+
+class TestSuite(unittest.TestCase):
+	def test_init(self):
+		cf = ConnectFour()
+		self.assertEqual(len(cf.position_to_disc), 0)
+		self.assertIsNone(cf.move)
+		self.assertEqual(cf.get_current_player(), 0)
+		self.assertIsNone(cf.get_winner())
+		self.assertEqual(ConnectFour.width, 7)
+		self.assertEqual(ConnectFour.height, 6)
+	
+	def test_place_disc(self):
+		# .......
+		# .......
+		# ..?....
+		# ..X....
+		# ..XOOO.
+		# .OXXXO.
+		positions = [
+			(2,0), (1,0),
+			(3,0), (3,1),
+			(4,0), (5,0),
+			(2,1), (4,1),
+			(2,2), (5,1)
+		]
+		cf = ConnectFour()
+		for i in range(len(positions)):
+			position = positions[i]
+			column = position[0]
+			cf.place_disc(column)
+			self.assertEqual(len(cf.position_to_disc), i+1)
+			self.assertEqual(cf.position_to_disc[position], i % 2)
+			self.assertEqual(cf.get_current_player(), (i+1) % 2)
+			self.assertIsNone(cf.get_winner())
+		cf.place_disc(2)
+		self.assertEqual(cf.get_winner(), 0)
+		
+		cf = ConnectFour()
+		try:
+			cf.place_disc(-1)
+			self.fail("Expected ValueError")
+		except ValueError:
+			pass
+		try:
+			cf.place_disc(ConnectFour.width)
+			self.fail("Expected ValueError")
+		except ValueError:
+			pass
+		for i in range(ConnectFour.height):
+			cf.place_disc(4)
+		try:
+			cf.place_disc(4)
+			self.fail("Expected ValueError")
+		except ValueError:
+			pass
+	
+	def test_get_heuristic_value(self):
+		string_list = [
+			".......",
+			".......",
+			".......",
+			".......",
+			"..OO...",
+			".OXXXX.",
+		]
+		cf = string_list_to_connect_four(string_list)
+		self.assertEqual(len(cf.position_to_disc), 7)
+		self.assertEqual(cf.get_current_player(), 1)
+		self.assertEqual(cf.get_heuristic_value(), 1000042-7)
+		self.assertEqual(cf.get_state_as_string(), "".join(string_list))
+		string_list = [
+			".......",
+			".......",
+			"O......",
+			"O......",
+			"O..XX..",
+			"O..XX..",
+		]
+		cf = string_list_to_connect_four(string_list)
+		self.assertEqual(len(cf.position_to_disc), 8)
+		self.assertEqual(cf.get_current_player(), 0)
+		self.assertEqual(cf.get_heuristic_value(), -1000042+8)
+		self.assertEqual(cf.get_state_as_string(), "".join(string_list))
+
+def string_list_to_connect_four(string_list):
+	symbol_to_player = {"X": 0, "O": 1}
+	cf = ConnectFour()
+	for y in range(ConnectFour.height):
+		line = string_list[ConnectFour.height-1-y]
+		for x in range(ConnectFour.width):
+			symbol = line[x]
+			try:
+				cf.position_to_disc[(x,y)] = symbol_to_player[symbol]
+			except KeyError:
+				pass
+	return cf
+
+if __name__ == "__main__":
+	unittest.main()
